26b3b83fc1b24697059c14f76674fa4dfc4a62c8
huan <huansonecacntt@gmail.com>
2024-05-23 19:51:42 -0300

    adicionando disparo de execoes

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Informe seu nome completo: ");
